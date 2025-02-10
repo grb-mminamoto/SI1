53e031348ab803241d6285c29bcc4258ff31d330
grb-mminamoto <marcio.minamoto@grbservices.com.br>
2025-02-10 11:43:38 -0300

    Add files via upload

---
+++ sqlExample.java
@@ -0,0 +1,34 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class SqlInjectionExample {
+    public static void main(String[] args) {
+        String username = "user";  // Usuário fornecido pelo usuário
+        String password = "password";  // Senha fornecida pelo usuário
+
+        try {
+            // Conectar ao banco de dados
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "root");
+
+            // Vulnerabilidade: SQL Injection
+            String query = "SELECT * FROM users WHERE username = '" + username + "' AND password = '" + password + "'";
+
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+
+            if (rs.next()) {
+                System.out.println("Login bem-sucedido");
+            } else {
+                System.out.println("Falha no login");
+            }
+
+            rs.close();
+            stmt.close();
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
